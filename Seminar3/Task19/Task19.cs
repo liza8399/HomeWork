b938eed2502cc1ce2e12081c87d042ddf5d843d7
Lizaveta <obnimashka007@gmail.com>
2022-10-02 00:11:37 +0300

    task19

--- Seminar3/Task19/Task19.cs
+++ Seminar3/Task19/Task19.cs
@@ -1,3 +1,7 @@
 ﻿Console.Write("Введите пятизначное число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-if ()
+if (n / 10000 == n % 10 && n % 10000 / 1000 == n % 100 / 10)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
